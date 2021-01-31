fbd013a819fc4fa7c2c51092ce7c365435795009
ramsharan0230 <ramsharan0230@gmail.com>
2021-01-31 12:18:53 +0545

    udated

--- resources/views/inspection/add.blade.php
+++ resources/views/inspection/add.blade.php
@@ -1,8 +1,35 @@
 @extends('layouts.admin.master')
 @section('title','Add Inspection')
 @push('stylesheets')
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+    div.finding_suggestions {
+      overflow-y:scroll; 
+      overflow-x:hidden; 
+      height:110px; 
+      border: 2px solid gray; 
+      border-radius: 5px;
+    }
+
+    ul.suggestions {
+      list-style-type: none;
+      margin-left: -22px;
+    }
+
+    .suggestions li{
+      cursor: pointer;
+      margin-bottom: 0px;
+    }
+
+    .li_suggestions {
+      border: 1px solid gray; 
+      border-radius: 2px; 
+      margin-left: -15px;
+      margin-top: 5px;
+      font-size: 12px
+    }
+  </style>
 @endpush
 @section('content')
     <section class="content-header">
@@ -23,6 +50,8 @@
 
      <!-- Main content -->
      <section class="content">
+      <form action="#" method="post">
+
         <div class="row">
           <div class="col-md-8">
             <div class="card card-primary">
@@ -38,14 +67,14 @@
                   <div class="row">
                       <div class="col-sm-8">
                         <div class="form-group">
-                            <label for="inputName">Locations</label>
-                            <input type="text" id="inputName" class="form-control">
+                            <label for="location">Location</label>
+                            <input type="text" id="location" class="form-control" name="location">
                         </div>
                       </div>
                       <div class="col-sm-4">
                         <div class="form-group">
                             <label for="inputStatus">Choose Location</label>
-                            <select class="form-control custom-select">
+                            <select class="form-control custom-select" id="location_choose">
                               <option selected disabled>Select one</option>
                               <option>Khalifa Kitchen</option>
                               <option>Store</option>
@@ -55,17 +84,25 @@
                       </div>
                   </div>
                   <div class="row">
-                    <div class="col-sm-6">
+                    <div class="col-sm-8">
                       <div class="form-group">
-                        <label for="inputDescription">Findings Description</label>
-                        <textarea id="inputDescription" class="form-control" rows="4"></textarea>
+                        <label for="findings">Findings Description</label>
+                        <textarea id="findings" class="form-control" rows="4" name="findings" placeholder="Type finding or problems"></textarea>
                       </div>
                     </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                          <label for="inputDescription">Proposed Corrective Actions</label>
-                          <textarea id="inputDescription" class="form-control" rows="4"></textarea>
-                        </div>
+                    <div class="col-sm-4" >
+                      <label for="finding_suggestions">Findings Suggestions</label>
+                      <div class="finding_suggestions">
+                        <ul class="suggestions">
+                          <li class="li_suggestions"><span class="t"><i class="fa fa-arrow-right"></i> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span></li>
+                          <li class="li_suggestions"><span class="t"><i class="fa fa-arrow-right"></i> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. </span></li>
+                          <li class="li_suggestions"><span class="t"><i class="fa fa-arrow-right"></i> Many desktop publishing packages and web page editors now use Lorem 
+                            Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</span></li>
+                          <li class="li_suggestions"><span class="t"><i class="fa fa-arrow-right"></i> The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</span></li>
+                          <li class="li_suggestions"><span class="t"><i class="fa fa-arrow-right"></i> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span></li>
+                        </ul>
+                      </div>
                     </div>
                 </div>
                 
@@ -73,14 +110,14 @@
                     <div class="col-sm-8">
                       <div class="form-group">
                             <label for="inputStatus">Accountibility</label>
-                            <input type="text" class="form-control" placeholder="Accountibility...">
+                            <input type="text" class="form-control" id="accountibility" placeholder="Accountibility...">
                       </div>
                     </div>
                     <div class="col-sm-4">
                         <div class="form-group">
                             <label for="inputStatus">Choose Accountibility</label>
-                            <select class="form-control custom-select">
-                                <option >Select one</option>
+                            <select class="form-control custom-select" id="accountibility1">
+                                <option selected disabled>Select one</option>
                                 <option>Production Department</option>
                                 <option>Store</option>
                                 <option>Maintenance</option>
@@ -105,23 +142,21 @@
                 </div>
               </div>
               <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-6">
-                    <div class="form-group">
-                        <label for="datepicker">Date</label>
-                        <div class='input-group date' id='datetimepicker1'>
-                            <input type="text" class="datepicker">
-                        </div>
-                    </div> 
-                    </div>
-                    <div class="col-sm-6">
-                    <div class="form-group">
-                        <label for="datepicker">Closing Date</label>
-                        <div class='input-group date' id='datetimepicker1'>
-                            <input type="text" class="datepicker">
-                        </div>
-                    </div>
-                    </div>
+                <div class="col-sm-12">
+                  <div class="form-group">
+                      <label for="datepicker1">Date</label>
+                      <div class='input-group date' id='datetimepicker1'>
+                          <input type="text" class="datepicker1">
+                      </div>
+                  </div> 
+                </div>
+                <div class="col-sm-12">
+                  <div class="form-group">
+                      <label for="datepicker">Closing Date</label>
+                      <div class='input-group date' id='datetimepicker1'>
+                          <input type="text" class="datepicker">
+                      </div>
+                  </div>
                 </div>
                 
                 <div class="form-group">
@@ -136,9 +171,10 @@
         </div>
         <div class="row">
           <div class="col-12">
-            <input type="submit" value="Create new Porject" class="btn btn-success float-right">
+            <input type="submit" value="Create new Inspection" class="btn btn-success float-right">
           </div>
         </div>
+      </form>
       </section>
       <!-- /.content -->
 
@@ -153,5 +189,25 @@
         { dateFormat: 'yy-mm-dd' }
     );
   } );
+
+  $(".datepicker1").datepicker({ dateFormat: 'yy-mm-dd' }).datepicker("setDate", new Date());
+
+
+  $('#accountibility1').change(function(){
+    $('#accountibility').val($('#accountibility1').val())
+  })
+
+  $('#location_choose').change(function(){
+    $('#location').val($('#location_choose').val())
+  })
+
+  $(document).ready(function() 
+ {
+    $('ul.suggestions li').click(function(e) { 
+      $('#findings').val($(this).find("span.t").text())
+    });
+ });
+
+
   </script>
   @endpush
