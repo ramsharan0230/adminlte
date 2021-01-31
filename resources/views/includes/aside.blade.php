fa4dc1674a703ef159c8c28cb600d9814c49f04d
ramsharan0230 <ramsharan0230@gmail.com>
2021-01-31 10:25:14 +0545

    updated

--- resources/views/includes/aside.blade.php
+++ resources/views/includes/aside.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ route('dashboard') }}" class="brand-link">
       <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Aa24Inspect</span>
     </a>
 
     <!-- Sidebar -->
@@ -75,24 +75,58 @@
             </ul>
           </li>
           
-          
+          <li class="nav-header"><strong>Reports</strong> </li>
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-chart-pie"></i>
+            <a href="{{ route('inspects.list') }}" class="nav-link">
+              <i class="nav-icon fas fa-file"></i>
               <p>
-                Reports
+                Submitted
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{ route('inspects.list') }}" class="nav-link">
+                <a href="{{ route('hygienes.list') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Lists</p>
+                  <p>PDF</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="{{ route('hygienes.list') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Excel</p>
                 </a>
               </li>
             </ul>
           </li>
+
+          <li class="nav-item has-treeview">
+            <a href="{{ route('inspects.list') }}" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Unsubmitteds
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="{{ route('hygienes.list') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>PDF</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="{{ route('hygienes.list') }}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Excel</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          
+
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
