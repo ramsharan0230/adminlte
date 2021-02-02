ee8b060234e99342b4a1be0e4a2a63935c4c683a
ramsharan0230 <ramsharan0230@gmail.com>
2021-02-02 22:05:10 +0545

    updated

--- resources/views/layouts/auth/master.blade.php
+++ resources/views/layouts/auth/master.blade.php
@@ -5,7 +5,7 @@
     <title>Aa24Inspect - @yield('title')</title>
     <meta name="description" content="Baguatte Admin">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="apple-touch-icon-precomposed" href="https://baguette-uae.com/wp-content/uploads/2019/12/Baguette_favicon.png">
     <link rel="icon" href="{{ asset('dist/img/avatar.png') }}" type="image/png">
 
